76dddf01d3dbe7fc21b2a5d2cd7fe17865ae8418
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2016-12-26 18:41:36 +0530

    initial

---
+++ js/app.js
@@ -0,0 +1,72 @@
+"use strict";
+var width;
+var height;
+var ctx;
+var element;
+var painterWorker;
+var imageData;
+
+addEventListener( 'DOMContentLoaded', function(){ 
+	$("#paintanim").hide( );
+$('#startBtn').prop('disabled', false);
+$('#stopBtn').prop('disabled', true);	
+element = document.getElementById("nartCanvas");
+ctx = element.getContext("2d");
+width = element.width;
+height = element.height;	
+
+}, false );
+
+function startPainting()
+{ //$("#duration").html("Painting...")
+	$("#paintanim").show( "slide", { direction: "left" },250 );
+ $("#duration").html("")	
+	if (painterWorker)
+	{
+		painterWorker.terminate();
+	}
+  $('#startBtn').prop('disabled', true);
+  $('#stopBtn').prop('disabled', false);
+    var networksize = $('#nneurons').val();
+	var nhidden = $('#nhlayer').val();
+	painterWorker = new Worker( 'js/neuralpaint.js' );
+	imageData = ctx.createImageData(width, height);
+	painterWorker.postMessage({
+    op: "start",
+	imgdata:imageData,
+	w:width,
+	h:height,
+	netsz:networksize,
+	hidsz:nhidden
+});
+
+painterWorker.onmessage = function( event ){
+//Got data paint it  
+
+
+ switch(event.data.status){
+        case "finished":
+		      $("#paintanim").hide( "slide", { direction: "right" },250 );
+			  $("#duration").html("Took "+parseInt(event.data.etime)+" seconds");
+			  $('#startBtn').prop('disabled', false);
+              $('#stopBtn').prop('disabled', true);	
+			  
+              break;
+        case "image":
+		      ctx.putImageData(event.data.imagedata, 0, 0);
+			  break;
+    } 
+};
+}
+function stopPainting()
+{
+$("#paintanim").hide( "slide", { direction: "right" },250 );
+ $("#duration").html("")	
+$('#startBtn').prop('disabled', false);
+$('#stopBtn').prop('disabled', true);	
+painterWorker.terminate();
+}
+
+
+  
+
